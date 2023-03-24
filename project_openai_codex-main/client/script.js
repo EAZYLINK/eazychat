78882d0c0f3100db4e3425f58b79a94c1337d476
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2023-03-24 20:02:21 +0100

    initial commit

---
+++ project_openai_codex-main/client/script.js
@@ -0,0 +1,146 @@
+import bot from './assets/bot.svg'
+import user from './assets/user.svg'
+
+const form = document.querySelector('form')
+const chatContainer = document.querySelector('#chat_container')
+const input = document.querySelector("#input")
+const field = document.querySelector('#textarea')
+
+function speaker(Data){
+    let speakData = new SpeechSynthesisUtterance(((Data.replace('\\n', '')).replace('bot', '')).replace(':', ''));
+    console.log(speakData)
+    speakData.voice = speechSynthesis.getVoices()[0];
+    speakData.rate = 1;
+    speechSynthesis.speak(speakData)
+}
+
+function inputVoices(){
+    var recognition = new webkitSpeechRecognition();
+    recognition.lang = 'en-US';
+    recognition.start();
+    recognition.onresult = function(event) {
+    var transcript = event.results[0][0].transcript;
+    field.textContent = transcript
+    }
+    recognition.addEventListener('audioend', async()=>{
+        if (field.textContent !== ''){
+            await handleSubmit()
+            field.textContent = ''
+        }
+       
+    })
+}
+
+let loadInterval
+
+function loader(element) {
+    element.textContent = ''
+    loadInterval = setInterval(() => {
+        // Update the text content of the loading indicator
+        element.textContent += '.';
+
+        // If the loading indicator has reached three dots, reset it
+        if (element.textContent === '....') {
+            element.textContent = '';
+        }
+    }, 300);
+}
+
+function typeText(element, text) {
+    let index = 0
+
+    let interval = setInterval(() => {
+        if (index < text.length) {
+            element.innerHTML += text.charAt(index)
+            index++
+        } else {
+            clearInterval(interval)
+        }
+    }, 20)
+}
+
+// generate unique ID for each message div of bot
+// necessary for typing text effect for that specific reply
+// without unique ID, typing text will work on every element
+function generateUniqueId() {
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
+
+    return `id-${timestamp}-${hexadecimalString}`;
+}
+
+function chatStripe(isAi, value, uniqueId) {
+    return (
+        `
+        <div class="wrapper ${isAi && 'ai'}">
+            <div class="chat">
+                <div class="profile">
+                    <img 
+                      src=${isAi ? bot : user} 
+                      alt="${isAi ? 'bot' : 'user'}" 
+                    />
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+            </div>
+        </div>
+    `
+    )
+}
+
+const handleSubmit = async () => {
+    const data = new FormData(form)
+
+    // user's chatstripe
+    chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+
+    // to clear the textarea input 
+    form.reset()
+
+    // bot's chatstripe
+    const uniqueId = generateUniqueId()
+    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+
+    // to focus scroll to the bottom 
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+
+    // specific message div 
+    const messageDiv = document.getElementById(uniqueId)
+
+    // messageDiv.innerHTML = "..."
+    loader(messageDiv)
+
+    const response = await fetch('http://localhost:5000/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            prompt: data.get('prompt')
+        })
+    })
+
+    clearInterval(loadInterval)
+    messageDiv.innerHTML = " "
+
+    if (response.ok && response.length !=0) {
+        const data = await response.json();
+        speaker(JSON.stringify(data))
+        const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
+
+        typeText(messageDiv, parsedData)
+    } else {
+        const err = await response.text()
+
+        messageDiv.innerHTML = "Something went wrong"
+        alert(err)
+    }
+}
+input.addEventListener('click', inputVoices)
+field.addEventListener('onkeyup', handleSubmit)
+form.addEventListener('submit', handleSubmit)
+form.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+        handleSubmit(e)
+    }
+})
